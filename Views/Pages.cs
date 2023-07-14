4608dab752970df6470eec37b3feee32c9299078
Cuong Vu Hung <cuongvuhung@gmail.com>
2023-07-14 17:16:42 +0700

    HTML PAGE

--- Views/Pages.cs
+++ Views/Pages.cs
@@ -8,10 +8,11 @@
             Content += "===================================================================================================" + "\n";
             Content += "|--------------------------------------EMPLOYEES MANAGER API--------------------------------------|" + "\n";
             Content += "===================================================================================================" + "\n";
-            Content += "GET  /employee                                          :Select all employee in database" + "\n";
+            Content += "GET  /employee    s                                      :Select all employee in database" + "\n";
             Content += "GET  /employee/{id}                                     :Select employee in database with id" + "\n";
             Content += "POST /employee/{name}/{fullname}/{password}/{role}      :Insert new employee EMPLOYEES MANAGER API" + "\n";
             Content += "PUT  /employee/{id}/{name}/{fullname}/{password}/{role} :Update new employee with data" + "\n";
+
         }
 
     }
